91657cbe0ce3bde57813c93be1a7fae87feeec94
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2025-08-07 19:46:06 +0900

    업데이트

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,12 +7,16 @@ public class Character : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Vector2 rawMoveVector2;
-    
+    [SerializeField] Quaternion targetRotation;
+
+
     private Rigidbody rb;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +25,10 @@ public class Character : MonoBehaviour
         Vector2 moveVector2 = rawMoveVector2 * speed * Time.deltaTime;
         Vector3 moveVector = new Vector3(moveVector2.x, 0, moveVector2.y);
         rb.MovePosition(rb.position + moveVector);
-        Quaternion targetRotation = Quaternion.LookRotation(moveVector);
+        targetRotation = Quaternion.LookRotation(moveVector);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
 
+        anim.SetFloat("Move", moveVector.magnitude);
     }
     public void OnAnimationStart()
     {
