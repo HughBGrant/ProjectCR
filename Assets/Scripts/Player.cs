2de4f79ad8ac2ac1a187832ed4538f8751307898
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2025-08-22 19:36:58 +0900

    애니메이션

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,54 +1,81 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _sprintSpeed;
+    [SerializeField] private float _currentSpeed;
+    [SerializeField] private float _jumpPower;
+    [SerializeField] private Vector2 _moveInput;
+    [SerializeField] private Vector3 _moveDirection;
 
-    [SerializeField] private float jumpPower;
-    [SerializeField] private bool isWalking;
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private Vector2 moveInput;
-    [SerializeField] private Vector3 moveDirection;
-    //[SerializeField] Quaternion targetRotation;
+    private bool _isWalking;
+    private bool _isSprinting;
+    private bool _isJumping;
+    private bool _shouldJump;
+    private bool _isAttacking;
+    private bool _isAttackHeld;
 
-    private Animator animator;
-    private Rigidbody rb;
+    private float _speedMultiplier = 1f;
+
+    private Animator _animator;
+    private Rigidbody _rb;
+    private Weapon _currentWeapon;
+
+    private Coroutine _attackCo;
+
+    private static readonly int _isWalkingHash = Animator.StringToHash("isWalking");
+    private static readonly int _isSprintingHash = Animator.StringToHash("isSprinting");
+    private static readonly int _isJumpingHash = Animator.StringToHash("isJumping");
+    private static readonly int _doJumpHash = Animator.StringToHash("doJump");
+    private static readonly int _doDodgeHash = Animator.StringToHash("doDodge");
+    private static readonly int _doSwapHash = Animator.StringToHash("doSwap");
+    private static readonly int _doShotHash = Animator.StringToHash("doShot");
+    private static readonly int _doSwingHash = Animator.StringToHash("doSwing");
+    private static readonly WaitForFixedUpdate _waitForFixedUpdate = new WaitForFixedUpdate();
+
     void Awake()
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
-    }
-    private void Update()
-    {
-        animator.SetBool("isMoving", moveDirection != Vector3.zero);
-
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
-
-        Vector3 velocity = moveDirection * speed;
-        velocity.y = rb.velocity.y;
-        rb.velocity = velocity;
-
-        //targetRotation = Quaternion.LookRotation(moveVec);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
-
-        if (moveDirection != Vector3.zero)
+        if (_shouldJump)
         {
-            transform.LookAt(transform.position + moveDirection);
+            _rb.velocity = new Vector3(_rb.velocity.x, _jumpPower, _rb.velocity.z);
+            _shouldJump = false;
         }
+        _moveDirection = new Vector3(_moveInput.x, 0f, _moveInput.y);
 
+        if (_isAttacking)
+        {
+            _moveDirection = Vector3.zero;
+        }
+        _currentSpeed = (_isWalking ? _walkSpeed : _sprintSpeed) * _speedMultiplier;
+        Vector3 moveVector = new Vector3(_moveDirection.x, 0f, _moveDirection.z) * _currentSpeed;
+        _rb.velocity = new Vector3(moveVector.x, _rb.velocity.y, moveVector.z);
+
+        if (_moveDirection.sqrMagnitude > 0.0001f)
+        {
+            transform.LookAt(transform.position + _moveDirection);
+        }
+        _isWalking = _moveDirection.sqrMagnitude > 0.0001f;
+
+        _animator.SetBool(_isWalkingHash, _isWalking);
+        _animator.SetBool(_isSprintingHash, _isSprinting);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-            isGrounded = true;
-        animator.SetBool("isJumping", false);
+        if (collision.gameObject.CompareTag(Tags.Ground))
+        {
+            _isJumping = false;
+            _animator.SetBool(_isJumpingHash, _isJumping);
+        }
     }
 
     //void OnCollisionExit(Collision collision)
@@ -57,18 +84,68 @@ public class Player : MonoBehaviour
     //        isGrounded = false;
     //}
 
-    public void OnMove(InputAction.CallbackContext context)
+    public void OnWalk(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        _moveInput = context.ReadValue<Vector2>();
+    }
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        _isSprinting = context.ReadValueAsButton();
     }
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        if (context.performed && !_isJumping)
         {
-            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-            isGrounded = false;
-            animator.SetTrigger("doJump");
-            animator.SetBool("isJumping", true);
+            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            _isJumping = true;
+            _animator.SetTrigger(_doJumpHash);
+            _animator.SetBool(_isJumpingHash, true);
         }
+    }
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isAttackHeld = true;
+
+            // 이미 루틴이 돌고 있지 않으면 시작
+            if (_attackCo == null)
+            {
+                _attackCo = StartCoroutine(AttackRoutine());
+            }
+        }
+        else if (context.canceled)
+        {
+            _isAttackHeld = false;
+
+            // 즉시 정지
+            if (_attackCo != null)
+            {
+                StopCoroutine(_attackCo);
+                EndAttack();
+            }
+        }
+    }
+    private IEnumerator AttackRoutine()
+    {
+        _isAttacking = true;
+
+        while (_isAttackHeld)
+        {
+            //_currentWeapon.Use();
+            //_animator.SetTrigger(_currentWeapon.WeaponType == WeaponType.Melee ? _doSwingHash : _doShotHash);
+
+            //yield return new WaitForSeconds(_currentWeapon.AttackSpeed);
+            //continue;
+            
+            yield return null;
+        }
+        EndAttack();
+    }
+    private void EndAttack()
+    {
+        _isAttacking = false;
+        _attackCo = null;
+
     }
 }
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+public static class Tags
+{
+    public const string Item = "Item";
+    public const string Weapon = "Weapon";
+    public const string Ground = "Ground";
+    public const string Wall = "Wall";
+
+}
