ff824b084af78ccf11f17ad378f2fab70ff52c07
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2025-09-19 19:49:11 +0900

    체력 바

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, IDamageable
 {
     [SerializeField]
+    private float maxHealth;
     private float currentHealth;
-    //public float HP => hp;
+    public float CurrentHealth { get { return currentHealth; } set { currentHealth = Mathf.Max(0, value); } }
     private Material material;
     private Coroutine hitCo;
     private float deathDestroyDelay = 2f;
+    [SerializeField]
+    private UI_Enemy healthBar;
 
     private void Awake()
     {
         material = GetComponent<MeshRenderer>().material;
     }
+    private void Start()
+    {
+        healthBar.SetMaxHealth(maxHealth);
+        CurrentHealth = maxHealth;
+    }
     public void TakeDamage(float damage)
     {
-        currentHealth -= damage;
-        Debug.Log($"체력 {damage} 감소. 현재 체력 {currentHealth}");
+        CurrentHealth -= damage;
+        healthBar.SetHealth(CurrentHealth / maxHealth);
+
+        Debug.Log($"체력 {damage} 감소. 현재 체력 {CurrentHealth}");
 
         if (hitCo != null)
         {
@@ -38,7 +48,7 @@ public class Enemy : MonoBehaviour, IDamageable
         material.color = Color.red;
         yield return new WaitForSeconds(0.5f);
 
-        if (currentHealth <= 0)
+        if (CurrentHealth <= 0)
         {
             Die();
         }
--- Assets/Scripts/UI_Enemy.cs
+++ Assets/Scripts/UI_Enemy.cs
@@ -1,18 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Image backHealthBar;
+    [SerializeField]
+    private Image frontHealthBar;
 
-    // Update is called once per frame
-    void Update()
+    public void SetMaxHealth(float maxValue)
     {
-        
+        backHealthBar.fillAmount = 1f;
+        frontHealthBar.fillAmount = 1f;
+
+    }
+    public void SetHealth(float value)
+    {
+        frontHealthBar.fillAmount = value;
+
+        StartCoroutine(DecreaseHealth());
+    }
+    IEnumerator DecreaseHealth()
+    {
+        float duration = 0.5f;
+        while (backHealthBar.fillAmount > frontHealthBar.fillAmount)
+        {
+            backHealthBar.fillAmount = Mathf.MoveTowards(
+                backHealthBar.fillAmount,
+                frontHealthBar.fillAmount,
+                (backHealthBar.fillAmount / duration) * Time.deltaTime
+            );
+            yield return null;
+        }
     }
 }
