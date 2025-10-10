495c2d0f25406d73c4536e3d410c8d6dc0a0fff5
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2025-10-10 18:08:03 +0900

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,40 +1,60 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
-    float[] stats = new float[(int)StatType.Count];
-    public void SetStat(StatType e, float value)
+    private float[] stats = new float[(int)StatType.Count];
+    public float this[StatType e]
     {
-        stats[(int)e] = value;
+        get => stats[(int)e];
+        set => stats[(int)e] = value;
     }
-    public float GetStat(StatType e)
-    {
-        return stats[(int)e];
-    }
+    private float currentHealth;
+    public float CurrentHealth { get { return currentHealth; } set { currentHealth = Mathf.Max(0, value); } }
 
     [SerializeField]
     private float maxHealth;
-    private float currentHealth;
-    public float CurrentHealth { get { return currentHealth; } set { currentHealth = Mathf.Max(0, value); } }
-    private Material material;
-    private Coroutine hitCo;
-    private float deathDestroyDelay = 2f;
     [SerializeField]
     private UI_HealthBar healthBar;
     [SerializeField]
     private GameObject DamageTextPrefab;
     [SerializeField]
     private Transform spawnPoint;
+    [SerializeField]
+    private MeshRenderer meshRenderer;
+    [SerializeField]
+    private Transform targetPoint;
+
+    private bool isChasing;
+    private Material material;
+    private Coroutine hitCo;
+    private NavMeshAgent navAgent;
+
+    private const float deathDestroyDelay = 2f;
 
     private void Awake()
     {
         material = GetComponent<MeshRenderer>().material;
+        //material = meshRenderer.material;
+        navAgent = GetComponent<NavMeshAgent>();
     }
     private void Start()
     {
         healthBar.SetMaxHealth(maxHealth);
         CurrentHealth = maxHealth;
+
+        StartCoroutine(StartChase());
+    }
+    private void Update()
+    {
+        //Target();
+
+        if (navAgent.enabled)
+        {
+            navAgent.SetDestination(targetPoint.position);
+            navAgent.isStopped = !isChasing;
+        }
     }
     public void TakeDamage(float damage)
     {
@@ -51,12 +71,25 @@ public class Enemy : MonoBehaviour, IDamageable
         hitCo = StartCoroutine(ReactToHit());
 
     }
+    private void Target()
+    {
+        float radius = 5f;
+        float range = 5f;
+
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, radius, transform.forward, range, LayerMask.GetMask("Player"));
+    }
     private void Die()
     {
         material.color = Color.gray;
 
 
         Destroy(gameObject, deathDestroyDelay);
+    }
+    private IEnumerator StartChase()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        isChasing = true;
     }
     private IEnumerator ReactToHit()
     {
@@ -72,4 +105,16 @@ public class Enemy : MonoBehaviour, IDamageable
             material.color = Color.white;
         }
     }
+
+    //public IEnumerator Attack(Enemy enemy)
+    //{
+    //    yield return YieldCache.WaitForSeconds(0.1f);
+    //    enemy.Rigidbody.AddForce(enemy.transform.forward * 20, ForceMode.Impulse);
+    //    enemy.HitBox.enabled = true;
+    //    yield return YieldCache.WaitForSeconds(0.5f);
+    //    enemy.Rigidbody.velocity = Vector3.zero;
+    //    enemy.HitBox.enabled = false;
+    //    yield return YieldCache.WaitForSeconds(2.0f);
+    //}
+
 }
